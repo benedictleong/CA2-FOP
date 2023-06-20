6c8b237cedafdd3901253dc40392b7cb751ad2ab
benedictleong <bleon903@gmail.com>
2023-06-20 20:14:41 +0800

    Update 2304128_Benedict Leong_Stage1.js

--- 2304128_Benedict Leong_Stage1.js
+++ 2304128_Benedict Leong_Stage1.js
@@ -1,1 +1,12 @@
+//name = Benedict Leong Jin Yew
+//class = DISM/1A/04
+//admin no. = P2304128
+var input = require('readline-sync');
+var movie1 = ['Black Panther:Wakanda Forever 2022','Adventure, Action,Drama, Fantasy, Sci-Fi,Thriller',161,'11 Nov 2022',[9,42]];
+var movie2 = ['Avatar: The Way of Water','Adventure, Sci-Fi',192,'16 Dec 2022',[4, 15]];
+var movie3 = ['Fast X','Crime, Action, Mystery,Thriller',43,'19 May 2023',[28, 60]];
+var movie4 = ['Ant-Man and the Wasp: Quantumania','Adventure, Action',120,'16 Feb 2023',[18,80]];
+var movie5 = ['M3GAN','Horror,Mystery,Thriller',102,'6 Jan 2023',[20,70]];
+var movieList = [movie1,movie2,movie3,movie4,movie5];
 
+console.log('Welcome to Silver Vintage Movie Review Program');
