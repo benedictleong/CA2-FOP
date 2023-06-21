9ce4c8665ca204fb6053214b95446c0e8c5ca2e2
benedictleong <bleon903@gmail.com>
2023-06-21 09:51:50 +0800

    fin

--- 2304128_BenedictLeong_Stage1.js
+++ 2304128_BenedictLeong_Stage1.js
@@ -8,6 +8,7 @@ var movie3 = ['Fast X','Crime, Action, Mystery,Thriller',43,'19 May 2023',[28, 6
 var movie4 = ['Ant-Man and the Wasp: Quantumania','Adventure, Action',120,'16 Feb 2023',[18,80]];
 var movie5 = ['M3GAN','Horror,Mystery,Thriller',102,'6 Jan 2023',[20,70]];
 var movieList = [movie1,movie2,movie3,movie4,movie5];
+
 console.log('Welcome to Silver Vintage Movie Review Program');
 var name = input.question('Please enter your name: ');
 console.log();
@@ -15,7 +16,7 @@ console.log('Hi ' + name + ', please select your choice:');
 console.log('\t' + '1. Display All Movies\n\t2. Coming Soon\n\t3. Coming Soon\n\t4. Coming Soon\n\t5. Coming Soon\n\t6. Exit');
 var reply = input.questionInt('\t' + '>> ');
 
-for (var g = 0; g < 5; g++) {
+for(var g = 0; g < 5; g++) {
     var hours = Math.floor(movieList[g][2]/60);
     var minutes = movieList[g][2] % 60;
     if(hours == 0){
@@ -26,12 +27,16 @@ for (var g = 0; g < 5; g++) {
     }
     else {
         movieList[g][2] = hours + 'h ' + minutes + 'm';
-    } 
+    }
+    movieList[g][4][1] = Math.round((movieList[g][4][1] / movieList[g][4][0]) * 10) / 10
 }
-
+var x = 0;
 do{
     if(reply == 1) {
-        
+        while(x < 5) {
+            console.log('Name \t\t: ' + movieList[x][0] + '\nGenre\t\t: ' + movieList[x][1] + '\nRunning Time\t: ' + movieList[x][2] + '\nRelease Date\t: ' + movieList[x][3] + '\nRating\t\t: ' + movieList[x][4][1] + ' (' + movieList[x][4][0] + ' voters)\n');
+            x++;
+        }
     } else if(reply > 1 && reply < 6) {
         console.log('Sorry, work in progress!');
         console.log();
